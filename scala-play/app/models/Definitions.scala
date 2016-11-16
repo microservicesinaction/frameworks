72ec69cfafd1ae64fa7e544940b433919782906b
Sofia Cole <sofia.cole@itv.com>
2016-11-16 15:18:50 +0000

    Improving json formatters

--- scala-play/app/models/Definitions.scala
+++ scala-play/app/models/Definitions.scala
@@ -1,57 +1,47 @@
 package models
 
-import play.api.libs.json.Json
-
-//todo get rid of needing to put "value" in json
+import play.api.libs.json.{Format, Json}
+import play.api.libs.json._
+import play.api.libs.functional.syntax._
 
 case class Price(value: Int, currency: String)
-
-object Price {
-  implicit val priceFormat = Json.format[Price]
-}
+object     Price { implicit val priceFormat = Json.format[Price] }
 
 case class Colour(value: String)
-
-object Colour {
-  implicit val colourFormat = Json.format[Colour]
-}
+object     Colour { implicit val colourFormat: Format[Colour] = Json.format[Colour] }
 
 case class AttributeTag(value: String)
-
-object AttributeTag {
-  implicit val attributeTagFormat = Json.format[AttributeTag]
-}
+object     AttributeTag { implicit val attributeTagFormat: Format[AttributeTag] = Json.format[AttributeTag] }
 
 case class Size(value: Int, region: String)
-
-object Size {
-  implicit val sizeFormat = Json.format[Size]
-}
+object     Size { implicit val sizeFormat = Json.format[Size] }
 
 case class State(value: String)
-
-object State {
-  implicit val stateFormat = Json.format[State]
-}
+object     State { implicit val stateFormat = Json.format[State] }
 
 case class Sku(value: String)
-
-object Sku {
-  implicit val skuFormat = Json.format[Sku]
-}
+object     Sku { implicit val skuFormat = Json.format[Sku] }
 
 //todo multiple tags
 case class Item(id: Int, price: Price, colour: Colour, tags: AttributeTag, size: Size, state: State, sku: Sku)
+object     Item {
 
-object Item {
-  implicit val itemFormat = Json.format[Item]
+  val itemReads: Reads[Item] = (
+      (JsPath \ "id").read[Int] and
+      (JsPath \ "price").read[Price] and
+      (JsPath \ "colour").read[String].map { value => Colour(value) } and
+      (JsPath \ "tags").read[String].map { value => AttributeTag(value) } and
+      (JsPath \ "size").read[Size] and
+      (JsPath \ "state").read[String].map { value => State(value) } and
+      (JsPath \ "sku").read[String].map { value => Sku(value) }
+    )(Item.apply _)
+
+  val itemWrites = Json.writes[Item]
+
+  implicit val itemFormat: Format[Item] =
+    Format(itemReads, itemWrites)
 }
 
 case class ItemRecord(id: Int, priceValue: Int, priceCurrency: String, colour: String, tags: String, sizeValue: Int, sizeRegion: String, state: String, sku: String) {
-  def toItem: Item =
-    Item(id = id, price = Price(priceValue, priceCurrency), colour = Colour(colour), tags = AttributeTag(tags), size = Size(sizeValue, sizeRegion), state = State(state), sku = Sku(sku))
-}
-
-object ItemRecord {
-  implicit val itemRecordFormat = Json.format[ItemRecord]
+  def toItem: Item = Item(id = id, price = Price(priceValue, priceCurrency), colour = Colour(colour), tags = AttributeTag(tags), size = Size(sizeValue, sizeRegion), state = State(state), sku = Sku(sku))
 }
--- scala-play/test/models/DefinitionsSpec.scala
+++ scala-play/test/models/DefinitionsSpec.scala
@@ -3,12 +3,15 @@ package models
 import org.scalatestplus.play.PlaySpec
 import play.api.libs.json.{JsValue, Json}
 import org.scalatest.Matchers._
+import play.api.mvc._
+
+
 
 class DefinitionsSpec extends PlaySpec {
 
   "Item Formats" should {
     "write to Json" in {
-      Json.toJson(ItemRecord(1, 100, "GBP", "Red", "Large Sizing", 10, "EUR", "NoIdea", "134-1435-53"))
+      Json.toJson(Item(1, Price(100, "GBP"), Colour("Red"), AttributeTag("Large Sizing"), Size(10, "EUR"), State("NoIdea"), Sku("134-1435-53")))
     }
 
     "get Item from Json" in {
@@ -21,27 +24,19 @@ class DefinitionsSpec extends PlaySpec {
            |    "value": 12,
            |    "currency": "GBP"
            |   },
-           |  "colour": {
-           |    "value": "colour"
-           |  },
-           |  "tags": {
-           |    "value": "tags"
-           |  },
+           |  "colour": "colour",
+           |  "tags": "tags",
            |  "size": {
            |    "value": 100,
            |    "region": "EU"
            |  },
-           |  "state": {
-           |    "value": "state"
-           |  },
-           |  "sku": {
-           |    "value": "sku"
-           |  }
+           |  "state": "state",
+           |  "sku": "sku"
            |}
            |
          """.stripMargin)
 
-      Json.fromJson[Item](json).get shouldBe Item(1, Price(12, "GBP"), Colour("colour"), AttributeTag("tags"), Size(100, "EU"), State("state"), Sku("sku"))
+      json.as[Item] shouldBe Item(1, Price(12, "GBP"), Colour("colour"), AttributeTag("tags"), Size(100, "EU"), State("state"), Sku("sku"))
     }
   }
 
